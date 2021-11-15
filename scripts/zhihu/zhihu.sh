be994fb23031874861556a5dbe08549096dab8f1
shenmengjia <shenmj13@gmail.com>
2021-11-15 17:17:05 +0800

    🦄 refactor: refactor zhihu and hexo

--- scripts/zhihu/zhihu.sh
+++ scripts/zhihu/zhihu.sh
@@ -2,8 +2,6 @@
 
 cd `dirname $0`
 PATH=$PATH:/usr/local/bin
-# sleep 0-99s
-sleep $[$RANDOM%100]
 python zhihu.py >> zhihu.txt
 echo "日期,粉丝数,赞同数,喜欢数,收藏数,文章数" > zhihu.csv
 tac zhihu.txt >> zhihu.csv
